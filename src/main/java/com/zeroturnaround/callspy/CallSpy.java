f64c2a5d0948baf082a34bcd8d4b9c6e2c97d881
Krzysztof Slonka <krzysztof.slonka@allegro.pl>
2018-08-12 08:18:44 +0200

    Add webclient calls

--- src/main/java/com/zeroturnaround/callspy/CallSpy.java
+++ src/main/java/com/zeroturnaround/callspy/CallSpy.java
@@ -1,8 +1,6 @@
 package com.zeroturnaround.callspy;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
+import javassist.*;
 
 import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
@@ -10,52 +8,41 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class CallSpy implements ClassFileTransformer {
-  @Override
-  public byte[] transform(//region other parameters
-                          ClassLoader loader,
-                          String className,
-                          Class<?> classBeingRedefined,
-                          ProtectionDomain protectionDomain,
-                          //endregion
-                          byte[] classfileBuffer) throws IllegalClassFormatException {
+  public byte[] transform(ClassLoader loader, String className, Class redefiningClass, ProtectionDomain domain, byte[] bytes) throws IllegalClassFormatException {
+    return transformClass(redefiningClass,bytes);
+  }
 
-    ClassPool cp = ClassPool.getDefault();
-    cp.importPackage("com.zeroturnaround.callspy");
-
-    //region filter agent classes
-    // we do not want to profile ourselves
-    if (className.startsWith("com/zeroturnaround/callspy")) {
-      return null;
-    }
-    //endregion
-
-    //region filter out non-application classes
-    // Application filter. Can be externalized into a property file.
-    // For instance, profilers use blacklist/whitelist to configure this kind of filters
-    if (!className.startsWith("com/zt")) {
-      return classfileBuffer;
-    }
-    //endregion
-
+  private byte[] transformClass(Class classToTransform, byte[] b) {
+    ClassPool pool = ClassPool.getDefault();
+    CtClass cl = null;
     try {
-      CtClass ct = cp.makeClass(new ByteArrayInputStream(classfileBuffer));
-
-      CtMethod[] declaredMethods = ct.getDeclaredMethods();
-      for (CtMethod method : declaredMethods) {
-        //region instrument method
-          method.insertBefore(" { " +
-                  "Stack.push();" +
-                  "Stack.log(\"" + className + "." + method.getName() + "\"); " +
-                  "}");
-          method.insertAfter("{ Stack.pop(); }", true);
-        //endregion
+      cl = pool.makeClass(new java.io.ByteArrayInputStream(b));
+      CtBehavior[] methods = cl.getDeclaredBehaviors();
+      for (int i = 0; i < methods.length; i++) {
+        if (methods[i].isEmpty() == false) {
+          changeMethod(methods[i]);
+        }
       }
-
-      return ct.toBytecode();
-    } catch (Throwable e) {
+      b = cl.toBytecode();
+    }
+    catch (Exception e) {
       e.printStackTrace();
     }
+    finally {
+      if (cl != null) {
+        cl.detach();
+      }
+    }
+    return b;
+  }
 
-    return classfileBuffer;
+  private void changeMethod(CtBehavior method) throws NotFoundException, CannotCompileException {
+    // !Modifier.isAbstract(method.getModifiers()) -- abstract methods can't be modified. If you get exceptions, then add this to the if statement.
+    // native methods can't be modified.
+
+    if (!Modifier.isNative(method.getModifiers()) && !Modifier.isAbstract(method.getModifiers())) {
+      String insertString = "System.out.println(\"started method " + method.getName() + "\");";
+      method.insertBefore(insertString);
+    }
   }
 }
---
+++ src/test/java/com/example/demo/WebClientFirstRequestTest.java
@@ -0,0 +1,113 @@
+package com.example.demo;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
+
+public class WebClientFirstRequestTest {
+
+    private static final String HOST = "localhost";
+    private static final String FOOS_URL = "/foos";
+    private static final String CONTENT_TYPE = "Content-type";
+    private WireMockServer wireMockServer;
+    private WireMock wireMock;
+    public String MY_URL;
+
+    @Before
+    public void setUp() throws Exception {
+        wireMockServer = new WireMockServer(options().dynamicPort());
+        wireMockServer.start();
+        wireMock = new WireMock(HOST, wireMockServer.port());
+        MY_URL = String.format("http://%s:%d/%s", HOST, wireMockServer.port(), FOOS_URL);
+    }
+
+    @Test
+    public void simpleRequest() throws Exception {
+
+        ResponseDefinitionBuilder responseBuilder = aResponse()
+                .withStatus(200)
+                .withBody("[]")
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON);
+
+        wireMock.register(get(FOOS_URL).willReturn(responseBuilder));
+
+        boolean a = true;
+        String s1 = "", s2 = "";
+        WebClient webClient = WebClient.create(MY_URL);
+
+        s1 = getString(webClient);
+
+        s2 = getString(webClient);
+
+        System.out.println(s1 + s2);
+
+        wireMock.verifyThat(getRequestedFor(urlEqualTo(FOOS_URL)));
+    }
+
+    private String getString(WebClient webClient) throws InterruptedException, java.util.concurrent.ExecutionException {
+        long before = System.nanoTime();
+        WebClient.RequestHeadersUriSpec<?> get = webClient.get();
+        long after = System.nanoTime();
+        System.out.println(String.format("webClinet.get(): %d ms", (after - before) / 1_000_000));
+
+        before = System.nanoTime();
+        WebClient.ResponseSpec retrieve = get.retrieve();
+        after = System.nanoTime();
+        System.out.println(String.format("retrieve(): %d ms", (after - before) / 1_000_000));
+
+        before = System.nanoTime();
+        Mono<String> stringMono = retrieve.bodyToMono(String.class);
+        after = System.nanoTime();
+        System.out.println(String.format("bodyToMono(): %d ms", (after - before) / 1_000_000));
+
+//        before = System.nanoTime();
+//        CompletableFuture<String> stringCompletableFuture = stringMono.toFuture();
+//        after = System.nanoTime();
+//        System.out.println(String.format("toFuture(): %d ms", (after - before) / 1_000_000));
+
+        before = System.nanoTime();
+//        String s = stringCompletableFuture.get();
+        String s = stringMono.block();
+        after = System.nanoTime();
+        System.out.println(String.format("block(): %d ms", (after - before) / 1_000_000));
+        System.out.println("--");
+        return s;
+    }
+
+    @Test
+    public void simpleRequest2() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseDefinitionBuilder responseBuilder = aResponse()
+                .withStatus(200)
+                .withBody("[]")
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON);
+
+        wireMock.register(get(FOOS_URL).willReturn(responseBuilder));
+
+        long start1 = System.nanoTime();
+        String body = restTemplate.getForEntity(MY_URL, String.class).getBody();
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        String body2 = restTemplate.getForEntity(MY_URL, String.class).getBody();
+        long end2 = System.nanoTime();
+
+        System.out.println(body + body2);
+
+        System.out.println(String.format("First took: %s ms", ((end1 - start1) / 1_000_000)));
+        System.out.println(String.format("Second took: %s ms", ((end2 - start2) / 1_000_000)));
+        System.out.println(String.format("Difference in time: %s ms", ((end1 - start1) - (end2 - start2)) / 1_000_000));
+
+        wireMock.verifyThat(getRequestedFor(urlEqualTo(FOOS_URL)));
+    }
+}
